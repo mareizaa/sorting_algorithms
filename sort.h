97810c4997c06706bed501235b8144396fbf67d4
mareizaa <marce.3210@gmail.com>
2021-06-15 18:54:34 -0500

    sort

--- sort.h
+++ sort.h
@@ -25,4 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void node_swap(listint_t *previous, listint_t *act);
+int partition(int *array, int low, int high, size_t size);
+void quick_r(int *array, int low, int high, size_t size);
+
 #endif
